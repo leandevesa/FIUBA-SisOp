6ca1d4af8661d659bdc3e4aa477932032cd56aaf
axl <voices117@gmail.com>
2017-04-22 12:36:27 -0300

    refactorizacion del instalador

--- instalador.sh
+++ instalador.sh
@@ -23,9 +23,9 @@ mostrar_ayuda()
 solicitar_directorio()
 {
     # solicita al usuario que ingrese un directorio y lo valida
-    # el primer parámetro debe ser el tipo directorio que se solicita (configuración, log, etc)
-    # el segundo parámetro debe ser el valor por defecto si el usuario no ingresa uno
-    # el tercer parámetro debe ser el nombre de la variable en la cual se guarda el resultado
+    # $1 debe ser el tipo directorio que se solicita (configuración, log, etc)
+    # $2 debe ser el valor por defecto si el usuario no ingresa uno
+    # $3 debe ser el nombre de la variable en la cual se guarda el resultado
 
     tipo_dir=$1
     default=$2
@@ -70,58 +70,49 @@ solicitar_directorio()
 
 guardar_config()
 {
-    valores_conf=(
-        dir_bins
-        dir_maes
-        dir_nove
-        dir_acep
-        dir_rech
-        dir_vali
-        dir_repo
-        dir_logs
-    )
-
     if [ ! -d "$DIR_CONF" ]; then
         mkdir -p $DIR_CONF
     fi
 
-    echo '#/bin/sh' > $ARCHIVO_CONF
+    echo '#!/bin/bash' > $ARCHIVO_CONF
     echo '' >> $ARCHIVO_CONF
 
-    for i in "${valores_conf[@]}"; do
+    for i in "${DIRECTORIOS[@]}"; do
         echo "$i=${!i}" >> $ARCHIVO_CONF
     done
 }
 
 obtener_datos_del_usuario()
 {
-    solicitar_directorio binarios $dir_bins dir_bins
-    solicitar_directorio maestros $dir_maes dir_maes
-    solicitar_directorio novedades $dir_nove dir_nove
-    solicitar_directorio aceptados $dir_acep dir_acep
-    solicitar_directorio rechazados $dir_rech dir_rech
-    solicitar_directorio validados $dir_vali dir_vali
-    solicitar_directorio reportes $dir_repo dir_repo
-    solicitar_directorio logs $dir_logs dir_logs
+    # por cada directorio solicita al usuario ingresar un path
+    for d in "${DIRECTORIOS[@]}"; do
+        solicitar_directorio $d ${!d} $d
+    done
 }
 
+# listado de directorios a solicitar
+DIRECTORIOS=(
+    binarios
+    maestros
+    novedades
+    aceptados
+    rechazados
+    validados
+    reportes
+    logs
+)
 
-# valores por defecto de directorios a definir por el usuario
-dir_bins="$DIR_BASE/bin"        # directorio de binarios
-dir_maes="$DIR_BASE/maestros"   # directorio de maestros
-dir_nove="$DIR_BASE/novedades"  # directorio de novedades
-dir_acep="$DIR_BASE/aceptados"  # directorio de aceptados
-dir_rech="$DIR_BASE/rechazados" # directorio de rechazados
-dir_vali="$DIR_BASE/validados"  # directorio de validados
-dir_repo="$DIR_BASE/reportes"   # directorio de reportes
-dir_logs="$DIR_BASE/logs"       # directorio de logs de comandos
-
-verificar_instalacion=0 # flag
+# crea la variable para cada directorio y con los valores por defecto
+for d in "${DIRECTORIOS[@]}"; do
+    eval "$d=$DIR_BASE/$d"
+done
 
 # carga el archivo de configuración
 if [ -f "$ARCHIVO_CONF" ] ; then
     source $ARCHIVO_CONF
 fi
+
+verificar_instalacion=0 # flag
 
 # manejo de los parametros de entrada
 while [ "$1" != "" ]; do
@@ -148,6 +139,7 @@ if ! ./dependencias.sh ; then
     exit 1
 fi
 
+# solicita al usuario los datos necesarios
 obtener_datos_del_usuario
 until ./pregunta.sh "Desea proceder con la instalación?" ; do
     obtener_datos_del_usuario
@@ -157,19 +149,8 @@ done
 guardar_config
 
 # crear directorios
-echo "se crea el directorio de binarios en $dir_bins"
-mkdir -p $dir_bins
-echo "se crea el directorio de binarios en $dir_maes"
-mkdir -p $dir_maes
-echo "se crea el directorio de binarios en $dir_nove"
-mkdir -p $dir_nove
-echo "se crea el directorio de binarios en $dir_acep"
-mkdir -p $dir_acep
-echo "se crea el directorio de binarios en $dir_rech"
-mkdir -p $dir_rech
-echo "se crea el directorio de binarios en $dir_vali"
-mkdir -p $dir_vali
-echo "se crea el directorio de binarios en $dir_repo"
-mkdir -p $dir_repo
-echo "se crea el directorio de binarios en $dir_logs"
-mkdir -p $dir_logs
+for d in "${DIRECTORIOS[@]}"; do
+    path=${!d}
+    echo "se crea el directorio de binarios en $path"
+    mkdir -p $path
+done
