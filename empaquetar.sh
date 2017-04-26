b9395d51232bfa4d0f12c10c8b5f433d4b7f83a9
Axel Lijdens <voices117@gmail.com>
2017-04-26 13:11:28 -0300

    agregando el script empaquetador

---
+++ empaquetar.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+NOMBRE_PAQUETE='Grupo02.tgz'
+DIR_PROYECTO=$1
+if [ -z "$DIR_PROYECTO" ] ; then
+    DIR_PROYECTO=.
+fi
+
+cd $DIR_PROYECTO
+
+if [ -f $NOMBRE_PAQUETE ] ; then
+    rm $NOMBRE_PAQUETE
+fi
+
+if [ -d /tmp/TP ] ; then
+    rm -R /tmp/TP
+fi
+
+mkdir /tmp/TP
+mkdir /tmp/TP/Grupo02
+mkdir /tmp/TP/dirconf
+cp instalador.sh /tmp/TP
+cp README.md /tmp/TP
+cp -r instalador /tmp/TP/
+
+tar -czvf $NOMBRE_PAQUETE -C /tmp/TP .
+
+rm -R /tmp/TP
